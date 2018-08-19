04df9eee25f57d5cd5f0747bc559af4ccc56e392
Samu Juvonen <samu.juvonen@gmail.com>
2018-08-19 21:32:59 +0300

    Print out lists of data for demo

--- f1db/statistics.js
+++ f1db/statistics.js
@@ -3,9 +3,24 @@ exports.configure = async (services) => {
 }
 
 exports.index = (request, view) => {
-  return this.db.query('SELECT DISTINCT year FROM races ORDER BY year').then((result) => {
+  let a = this.db.query('SELECT DISTINCT year FROM races ORDER BY year');
+  let b = this.db.query(`
+    SELECT DISTINCT a.id, a.first_name, a.last_name, d.id team_id, d.name team_name
+    FROM drivers a
+      INNER JOIN race_results b
+        ON a.id = b.driver_id
+      INNER JOIN races c
+        ON b.race_id = c.id
+      INNER JOIN teams d
+        ON b.team_id = d.id
+    WHERE c.year = (SELECT MAX(year) FROM races)
+    ORDER BY team_name, a.last_name, a.first_name
+  `);
+
+  return Promise.all([a, b]).then(([years, drivers]) => {
     let v = view('f1db/index', {
-      years: result.map(r => r.year)
+      years: years.map(r => r.year),
+      drivers
     });
 
     return v;
